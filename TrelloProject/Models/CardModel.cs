fc8473023b33642ec81b569bd590a2ce1aa54bcd
Uchral Banzragch <uchral.banzragch@lombiq.com>
2022-05-29 16:44:09 +0200

    Added CardModel

---
+++ TrelloProject/Models/CardModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TrelloProject.Models
+{
+    public class CardModel
+    {
+        public string id { get; set; }
+        public string Status { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string OwnerEmail { get; set; }
+    }
+}
