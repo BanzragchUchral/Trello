189acfd6d599d3c944a3c03f21c3a41fedd8ff60
Uchral Banzragch <uchral.banzragch@lombiq.com>
2022-05-30 17:54:45 +0200

    Using select for status

--- TrelloProject/Models/CardModel.cs
+++ TrelloProject/Models/CardModel.cs
@@ -1,9 +1,11 @@
-﻿namespace TrelloProject.Models
+﻿using TrelloProject.Constants;
+
+namespace TrelloProject.Models
 {
     public class CardModel
     {
         public string id { get; set; }
-        public string Status { get; set; }
+        public Status Status { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string OwnerEmail { get; set; }
