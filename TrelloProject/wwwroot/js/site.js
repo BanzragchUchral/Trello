8622c2715be4aaed9e34fa5c7d3bb94bde83d472
Uchral Banzragch <uchral.banzragch@lombiq.com>
2022-05-30 19:17:23 +0200

    Moved script to site.js and fixed card spilling

--- TrelloProject/wwwroot/js/site.js
+++ TrelloProject/wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function init() {
+    dragula([document.querySelector("#pending"),
+    document.querySelector("#progress"),
+    document.querySelector("#blocked"),
+    document.querySelector("#done")], {
+        revertOnSpill: true
+    })
+    .on('drop', function (el, current, prev) {
+        if (current.id !== prev.id) {
+            var data = new FormData();
+            data.append('id', $(el)[0].children[0].name);
+            data.append('status', current.id);
 
-// Write your JavaScript code.
+            var request = new XMLHttpRequest();
+            var url = "/Home/Move";
+            request.open("POST", url, true);
+            request.send(data);
+        }
+    });
+};
+
+function showModal(url) {
+    $.get(url,
+        function (data) {
+            $('.modal-body').html(data);
+        });
+
+    $("#myModal").modal("show");
+};
